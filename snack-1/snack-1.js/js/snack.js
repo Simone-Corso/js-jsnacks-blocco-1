c889e7e308024f8aaafeec6430f445ec1d2b39fe
Simone-Corso <scorso26@gmail.com>
2023-11-10 11:50:00 +0100

    ho messo un prompt con la richiesta di quanti anni hai

--- snack-1/snack-1.js/js/snack.js
+++ snack-1/snack-1.js/js/snack.js
@@ -1,1 +1,5 @@
 //chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+
+const FirstYears = prompt('Quanti anni hai?')
+const SecondYears = prompt('Quanti anni hai?')
