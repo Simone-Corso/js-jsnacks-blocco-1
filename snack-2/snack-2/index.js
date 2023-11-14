217825e4ad409138e31b56b1efd0fe7acd7b2e89
Simone-Corso <scorso26@gmail.com>
2023-11-14 12:07:49 +0100

    ho inserito il const con elemento selezionato div wrapper e poi messo for per ciclo di 6 volte

---
+++ snack-2/snack-2/index.js
@@ -0,0 +1,12 @@
+
+
+//Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
+
+//* selezioniamo l'elemento che ci serve.
+
+const wrapperElement = document.querySelector('div.my_wrapper');
+
+for( let i = 1; i <=6; i++){
+    
+}
+
