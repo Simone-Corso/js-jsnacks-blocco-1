f1aa6d2d0066e27bc7610eba87d4ec5dce57d75d
Simone-Corso <scorso26@gmail.com>
2023-11-16 16:29:58 +0100

    correzione ho messo console.log come array

--- snack-b/script.js
+++ snack-b/script.js
@@ -1,16 +1,15 @@
 //Chiedi un numero di 7 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
 
-//Dobbiamo chiedere 7 cifre all'utente
-// - creare sette prompt per chiedere 7 cifre
+//Dobbiamo chiedere un numero di 7 cifre all'utente
 
-const primaCifra = prompt('Scrivimi la prima cifra');
-const secondaCifra = prompt('Scrivimi la seconda cifra');
-const terzaCifra = prompt('Scrivimi la terza cifra');
-const quartaCifra = prompt('Scrivimi la quarta cifra');
-const quintaCifra = prompt('Scrivimi la quinta cifra');
-const sestaCifra = prompt('Scrivimi la sesta cifra');
-const settimaCifra = prompt('Scrivimi la settima cifra');
+const chiediCifra = prompt('inserisci una cifra di 7 numeri');
 
-console.log(primaCifra + secondaCifra + terzaCifra + quartaCifra + quintaCifra + sestaCifra + settimaCifra );
-//calcolare la somma di tutte le cifre che ha messo l'utente
+console.log(chiediCifra[0]);
+console.log(chiediCifra[1]);
+console.log(chiediCifra[2]);
+console.log(chiediCifra[3]);
+console.log(chiediCifra[4]);
+console.log(chiediCifra[5]);
+console.log(chiediCifra[6]);
+
 //Mettere il risultato della somma ottenuta da 7 cifre.
