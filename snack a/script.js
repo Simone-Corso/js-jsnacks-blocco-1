a4f12773c9e261e320647085ae944cbf562227d0
Simone-Corso <scorso26@gmail.com>
2023-11-16 15:08:15 +0100

    ho creato 6 prompt per l'utente

--- snack a/script.js
+++ snack a/script.js
@@ -1,7 +1,7 @@
 //Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
 
 //1 - si crea un array vuota ( let vuota = [])
-//2 - chiedere all'utente 6 volte un numero ---> 
+//2 - chiedere all'utente 6 volte un numero ---> creando 6 prompt
 //3 - se compare dispari, devo metterlo nell array.
 
 
@@ -12,3 +12,9 @@ console.log(vuota)
 
 // 2 - chiediamo 6 volte all'utente un numero
 
+const numeroPrimo = parseInt( prompt ('dammi un numero'));
+const numeroSecondo = parseInt( prompt ('dammi un secondo numero'));
+const numeroTerzo = parseInt( prompt ('dammi un terzo numero'));
+const numeroQuarto = parseInt( prompt ('dammi un quarto numero'));
+const numeroQuinto = parseInt( prompt ('dammi un quinto numero'));
+const numeroSesto = parseInt( prompt ('dammi un sesto numero'));
