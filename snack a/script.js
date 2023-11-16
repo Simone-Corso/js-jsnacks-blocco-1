7f38170939af714d8527da5e6e39aee6dc384cf4
Simone-Corso <scorso26@gmail.com>
2023-11-16 15:00:38 +0100

    la scaletta per poter realizzare il primo passaggio, creare una array vuota

---
+++ snack a/script.js
@@ -0,0 +1,9 @@
+//Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+//1 - si crea un array vuota ( let vuota = [])
+//2 - chiedere all'utente 6 volte un numero ---> 
+//3 - se compare dispari, devo metterlo nell array.
+
+
+// 1 - si crea un array vuota ( let vuota = [])
+
