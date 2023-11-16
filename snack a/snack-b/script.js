e71a0f8dfe35e83cfd6798895b06e842568e5d0e
Simone-Corso <scorso26@gmail.com>
2023-11-16 15:27:18 +0100

    Ho inserito sette prompt

--- snack a/snack-b/script.js
+++ snack a/snack-b/script.js
@@ -2,5 +2,13 @@
 
 //Dobbiamo chiedere 7 cifre all'utente
 // - creare sette prompt per chiedere 7 cifre
+
+const primaCifra = prompt('Scrivimi la prima cifra');
+const secondaCifra = prompt('Scrivimi la seconda cifra');
+const terzaCifra = prompt('Scrivimi la terza cifra');
+const quartaCifra = prompt('Scrivimi la quarta cifra');
+const quintaCifra = prompt('Scrivimi la quinta cifra');
+const sestaCifra = prompt('Scrivimi il sesto cifra');
+const settimaCifra = prompt('Scrivimi la settima cifra');
 //calcolare la somma di tutte le cifre che ha messo l'utente
 //Mettere il risultato della somma ottenuta da 7 cifre.
