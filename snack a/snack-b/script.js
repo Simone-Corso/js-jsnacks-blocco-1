6271f704ea6d1b67c66c03f9b4f34f55f482b79f
Simone-Corso <scorso26@gmail.com>
2023-11-16 15:24:44 +0100

    inserito la scaletta per realizzare l'esercizio snack-b

---
+++ snack a/snack-b/script.js
@@ -0,0 +1,6 @@
+//Chiedi un numero di 7 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
+
+//Dobbiamo chiedere 7 cifre all'utente
+// - creare sette prompt per chiedere 7 cifre
+//calcolare la somma di tutte le cifre che ha messo l'utente
+//Mettere il risultato della somma ottenuta da 7 cifre.
